fa964ce80206ed4c684ef86eb6ff1f6ded16eb63
kunevi <rotherexram@gmail.com>
2023-04-10 16:57:25 +0200

    authentication and config!!!!!

--- src/commands/auth/login/mod.rs
+++ src/commands/auth/login/mod.rs
@@ -3,23 +3,53 @@ mod browser_auth;
 use anyhow::Result;
 use clap::Parser;
 
+use crate::{util::types::User, config::Config};
+
 use self::browser_auth::browser_login;
 
 #[derive(Debug, Parser, Default, PartialEq, Eq)]
 #[clap(about = "Login to whop")]
-pub struct Options {
-    #[clap(long = "token", help = "Whop Account Token")]
-    pub token: Option<String>,
-    #[clap(long = "email", help = "Whop Account Email")]
-    pub email: Option<String>,
-    #[clap(long = "password", help = "Whop Account Password")]
-    pub password: Option<String>,
+pub struct Options {}
+
+pub async fn run(_options: Options) -> Result<()> {
+    let config = Config::load();
+    
+    if config.get_user().is_some() {
+        println!("You're already authenticated! Run {} whoami to check your info!", crate::vars::CLI_NAME);
+    } else {
+        let token = browser_login().await?; // uses whop's OAuth to get users access token. required to be able to call https://api.whop.com/api/v2/me endpoints
+
+        handle_token(token).await?;
+    }
+
+    Ok(())
 }
 
-pub async fn run(options: Options) -> Result<()> {
-    // TODO: implement
+async fn handle_token(token: String) -> Result<()> {
+    let client = reqwest::Client::new();
 
-    let token = browser_login().await?;
+    let res = client.get("https://api.whop.com/api/v2/me")
+        .bearer_auth(token.clone())
+        .send()
+        .await?;
+
+    let json: serde_json::Value = res.json().await?;
+    let username = json["username"].as_str().unwrap_or_default().to_string();
+    let id = json["id"].as_str().unwrap_or_default().to_string();
+
+    let user_data = User {
+        id,
+        username,
+        access_token: token
+    };
+
+    let mut config = Config::load();
+
+    config.set_user(user_data);
+
+    config.save()?;
+
+    println!("🚀 Successfully authenticated to whop!");
 
     Ok(())
 }
